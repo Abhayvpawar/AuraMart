65851d851c939a36f84e5eecc5967385084edb1b
abhayvpawar <abhaypawar.cs.04@gmail.com>
2025-03-18 00:10:01 +0530

    Updated settings for Render HTTPS 2

--- E_Comm/settings.py
+++ E_Comm/settings.py
@@ -29,11 +29,11 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["AuraMart.onrender.com"]
+ALLOWED_HOSTS = ["auramart.onrender.com"]
 DATABASES = {
     'default': dj_database_url.config(default='sqlite:///db.sqlite3')
 }
-SECURE_SSL_REDIRECT = True
+SECURE_SSL_REDIRECT = False
 CSRF_COOKIE_SECURE = True
 SESSION_COOKIE_SECURE = True
 
